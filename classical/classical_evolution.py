78738f12bca3d390649ce3374e9837bf5cb46972
Sam Cochran <samcochran87@gmail.com>
2023-10-14 18:58:47 -0400

    minor edit to implementation to match jason's setup for easier comparison

--- classical/classical_evolution.py
+++ classical/classical_evolution.py
@@ -6,47 +6,81 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-v0_8')
 
 
-def time_evolve(potential, initial_wave_function, N, L, K, T, f, ord=2, D=1/2, h_bar=1, m=1):
+def time_evolve_1(potential, initial_wave_function, N, L, K, T, f, D=1/2, h_bar=1, m=1):
     dx, dt = 2*L/N, T/K
     x_grid = np.arange(-L, L - dx/2, dx)
-    t_grid = np.arange(0, T - dt/2, dt)
+    t_grid = np.arange(0, T + dt/2, dt)
 
     # Initialize the initial wave function
     psi = initial_wave_function(x_grid)
 
-    states = []
+    out = True
+    states = [psi]
     progress = tqdm(total = K, desc='working on time evolution')
 
-    # Propogate potential half step to start
-    if ord == 2:
-        pass
-    elif ord == 1:
-        pass
-    else:
-        raise(Exception('Only 1st and 2nd order implemented.'))
-
     for i in range(K):
-        # Propogate potential (half) step
-        if ord == 2:
-            psi = psi*np.exp(-1j*(potential(x_grid) + f(np.abs(psi)**2))*dt/2/h_bar)
-        else:
-            psi = psi*np.exp(-1j*(potential(x_grid) + f(np.abs(psi)**2))*dt/h_bar)
-
         # Propogate kinetic
         psi = fft(psi)
         p = 2*np.pi*fftfreq(N, d=dx)  # Momentum grid
         psi = psi*np.exp(-1j*D*dt*h_bar*p**2/m)
         psi = ifft(psi)
 
-        # Propogate potential half step if second order
-        if ord == 2:
-            psi = psi*np.exp(-1j*(potential(x_grid) + f(np.abs(psi)**2))*dt/2/h_bar)
+        # Propogate potential
+        ff = f(np.abs(psi)**2)
+        psi = psi*np.exp(-1j*(potential(x_grid) + ff)*dt/h_bar)
 
-        states.append(psi)
+        if out:
+            states.append(psi)
         progress.update(1)
     progress.close()
 
     states = np.array(states)
+    return states, t_grid, x_grid
+
+def time_evolve_2(potential, initial_wave_function, N, L, K, T, f, D=1/2, h_bar=1, m=1):
+    dx, dt = 2*L/N, T/K
+    x_grid = np.arange(-L, L - dx/2, dx)
+    t_grid = np.arange(0, T + dt/2, dt)
+
+    # Initialize the initial wave function
+    psi = initial_wave_function(x_grid)
+
+    out = True
+    states = [psi]
+    progress = tqdm(total = K, desc='working on time evolution')
+
+    # Propogate potential half step to start
+    ff = f(np.abs(psi)**2)
+    psi = psi*np.exp(-1j*(potential(x_grid) + ff)*dt/2/h_bar)
+
+    for i in range(K - 1):
+        # Propogate kinetic
+        psi = fft(psi)
+        p = 2*np.pi*fftfreq(N, d=dx)  # Momentum grid
+        psi = psi*np.exp(-1j*D*dt*h_bar*p**2/m)
+        psi = ifft(psi)
+
+        # Propogate potential
+        ff = f(np.abs(psi)**2)
+        psi = psi*np.exp(-1j*(potential(x_grid) + ff)*dt/h_bar)
+
+        if out:
+            states.append(psi.copy()*np.exp(1j*(potential(x_grid) + ff)*dt/2/h_bar))
+        progress.update(1)
+
+    # Propogate kinetic
+    psi = fft(psi)
+    p = 2*np.pi*fftfreq(N, d=dx)  # Momentum grid
+    psi = psi*np.exp(-1j*D*dt*h_bar*p**2/m)
+    psi = ifft(psi)
+    psi = psi*np.exp(-1j*(potential(x_grid) + ff)*dt/2/h_bar)
+
+    if out:
+        states.append(psi)
+    progress.update(1)
+    progress.close()
+
+    states = np.array(states, dtype='complex')
     return states, t_grid, x_grid
 
 
